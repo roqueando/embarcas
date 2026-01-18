6400893453430381f0ffa5692677f0639f5c0e88
Vitor Roque <vitor.roquep@gmail.com>
2026-01-18 01:21:07 -0300

    fix: all good from here

--- src/main.c
+++ src/main.c
@@ -4,7 +4,58 @@
 #include "ssd1780/ssd1780.h"
 #include <util/delay.h>
 
+#define SCL_PIN PB2
+#define SDA_PIN PB0
+
 int main(void) {
+    uart_t uart = uart_init((uart_config_t){
+        .tx_pin = PB3,
+        .rx_pin = PB1,
+        .baudrate = 9600
+    });
+
+    i2c_t i2c = i2c_init((i2c_config_t){
+        .sda_pin = SDA_PIN,
+        .scl_pin = SCL_PIN,
+        .timeout_us = 10000
+    });
+
+    uart_puts(&uart, "I2C Scanner:\r\n");
+    uint8_t data;
+
+
+    for (uint8_t addr = 0x00; addr < 0x80; addr++) {
+        i2c_address(&i2c, addr, 0);
+
+        if (i2c_write_byte_wait_ack(&i2c, addr) == I2C_OK) {
+            uint8_t found = 1;
+            uart_puts(&uart, "Device found at: 0x");
+            uart_putc(&uart, (addr >> 4) | 0x0F);
+            uart_puts(&uart, "\r\n");
+
+            for (uint8_t reg = 0; reg < 0x10; reg++) {
+                if (i2c_read_reg(&i2c, addr, reg, &data) != I2C_OK) {
+                    uart_puts(&uart, "  R");
+                    uart_putc(&uart, reg);
+                    found = 1;
+                }
+            }
+
+            if (found) {
+                uart_puts(&uart, "\r\n");
+            }
+        } else {
+            uart_puts(&uart, ".");
+        }
+
+        _delay_ms(10);
+    }
+
+    uart_puts(&uart, "\r\nScan complete.\r\n");
+
+    while (1) {
+    }
+    /*
     i2c_t i2c;
     ssd1780_t display;
     ssd1780_status_t status;
@@ -64,4 +115,5 @@ int main(void) {
 
         status = ssd1780_draw_box(&display, 10, 10, 117, 53, SSD1780_COLOR_WHITE);
     }
+    */
 }
